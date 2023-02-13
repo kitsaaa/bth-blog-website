40ae7fdaa89c995dead2fbe0bdc698da0b19d9b5
Nikita Korolkov <nikitakorolkov@Nikitas-MacBook-Pro.local>
2023-02-13 17:32:16 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BlogComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
